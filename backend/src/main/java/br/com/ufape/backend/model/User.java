59f923f39c4fa50fa6373715c4ba1c7e8be2fa16
Felifelps <felifelps.dev@gmail.com>
2026-08-01 22:13:39 -0300

    feat: adiciona entidade User

---
+++ backend/src/main/java/br/com/ufape/backend/model/User.java
@@ -0,0 +1,88 @@
+package br.com.ufape.backend.model;
+
+import br.com.ufape.backend.enums.UserRole;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @SequenceGenerator(name="user_id_seq", allocationSize=1)
+    @GeneratedValue(generator="user_id_seq", strategy=GenerationType.SEQUENCE)
+    @Column(name="id", updatable=false)
+    private Long id;
+
+    @Column(nullable=false)
+    @NotBlank(message="Nome é obrigatório")
+    private String name;
+
+    @Column(nullable=false, unique=true)
+    @NotBlank(message="Email é obrigatório")
+    @Email(message="Email em formato inválido")
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable=false)
+    @NotNull(message="O cargo do usuário é obrigatório")
+    private UserRole role;
+
+    @Column(nullable=false)
+    @NotBlank(message="A senha é obrigatória")
+    @Size(min=6, message="A senha deve ter no mínimo 6 caracteres")
+    @JsonProperty(access=JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    public User() {}
+
+    public User(String name, String email, UserRole role, String password) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
