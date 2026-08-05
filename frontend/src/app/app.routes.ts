3f21c9d040c72319ad7c2b2e59648fa5a7f7ba58
Felifelps <felifelps.dev@gmail.com>
2026-08-05 18:14:27 -0300

    feat: adiciona guard de redirecionamento na rota raiz

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,30 @@
 import { Routes } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
+import { rootGuard } from './core/guards/root.guard';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: 'register',
-        pathMatch: 'full'
-    },
-    {
-        path: 'register',
-        loadComponent: () => import('./features/auth/register/register.component')
-            .then(m => m.RegisterComponent)
-    },
-    {
-        path: 'login',
-        loadComponent: () => import('./features/auth/login/login.component')
-            .then(m => m.LoginComponent)
-    },
-    {
-        path: 'area-logada',
-        canActivate: [authGuard],
-        loadComponent: () => import('./features/authenticated/logged-area/logged-area.component')
-            .then(m => m.LoggedAreaComponent)
-    }
+  {
+    path: '',
+    canActivate: [rootGuard],
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register.component').then((m) => m.RegisterComponent),
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'area-logada',
+    canActivate: [authGuard],
+    loadComponent: () =>
+      import('./features/authenticated/logged-area/logged-area.component').then(
+        (m) => m.LoggedAreaComponent,
+      ),
+  },
 ];
