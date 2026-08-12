eac185c06356ea1c0e08fb661ed42e2f92342c99
Felifelps <felifelps.dev@gmail.com>
2026-08-12 20:31:40 -0300

    feat: adicionar rotas de busca e detalhe de serviço

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -4,6 +4,8 @@ import Register from '../pages/Register/Register';
 import Home from '../pages/Home/Home';
 import PrivateRoute from '../routes/PrivateRoute';
 import BecomeProvider from '../pages/BecomeProvider/BecomeProvider';
+import Servicos from '../pages/Servicos/Servicos';
+import ServicoDetalhe from '../pages/ServicoDetalhe/ServicoDetalhe';
 
 export default function AppRoutes() {
   return (
@@ -16,6 +18,8 @@ export default function AppRoutes() {
       {/* Rotas Protegidas (Todas as rotas filhas exigem autenticação) */}
       <Route element={<PrivateRoute allowedRoles={['USER', 'ADMIN', 'PRESTADOR']} />}>
         <Route path="/become-provider" element={<BecomeProvider />} />
+        <Route path="/servicos" element={<Servicos />} />
+        <Route path="/servicos/:id" element={<ServicoDetalhe />} />
       </Route>
 
       {/* Redirecionamento para rota não encontrada ou padrão */}
