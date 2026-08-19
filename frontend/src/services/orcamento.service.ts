376b7c08e25d4d1c5fd27d756f25693f980e325e
Joran Vinicíus Silveira Lage <joranvinicius@gmail.com>
2026-08-18 23:06:29 -0300

    Adicionei metodos para Orcamentos
    Metodo de listar Orcamentos recebidos e metodo de responderOrcamento

--- frontend/src/services/orcamento.service.ts
+++ frontend/src/services/orcamento.service.ts
@@ -1,10 +1,23 @@
 import api from './api';
 import type { OrcamentoRequest } from '../models/orcamento-request.model';
 import type { OrcamentoResponse } from '../models/orcamento-response.model';
+import type { OrcamentoRespostaRequest } from '../models/orcamento-resposta-request.model';
 
 class OrcamentoService {
   async solicitar(data: OrcamentoRequest): Promise<OrcamentoResponse> {
     const response = await api.post<OrcamentoResponse>('/orcamentos', data);
+    return response.data;
+  }
+
+  //1. Listar orçamentos recebidos
+  async listarOrcamentosRecebidos(): Promise<OrcamentoResponse[]> {
+    const response = await api.get<OrcamentoResponse[]>('/orcamentos/recebidos');
+    return response.data;
+  }
+
+  //2. Responder a um orçamento
+  async responderOrcamento(id: number, data: OrcamentoRespostaRequest): Promise<OrcamentoResponse> {
+    const response = await api.post<OrcamentoResponse>(`/orcamentos/${id}/responder`, data);
     return response.data;
   }
 }
